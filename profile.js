07a7bf8285a28ff18a7811a4d58258762ed9e750
unknown <orfali.murhaf@web.de>
2018-12-13 14:07:28 +0100

    chane js file and made a function to reduce the code lines number

--- profile.js
+++ profile.js
@@ -23,99 +23,40 @@ $(document).ready(ev =>{
    counter = 0;
   })
 
+let popup = function(img ,text){
+  let newSection = document.createElement('SECTION');
+  let closeSpan = document.createElement('SPAN');
+  let newH1 = document.createElement('H1');
+  let pic = document.createElement('IMG');
+  pic.src = `pictures/${img}.jpg`;
+  newSection.classList.add('onthefly');
+  closeSpan.innerText = 'X';
+  newH1.innerText =`${text}`;
+ newSection.append( closeSpan, newH1, pic);
+ $('body').append(newSection);
+ $('.onthefly span').click(function (ev){
+ $(this).parent().remove();
+})
+}
+
   $('#education').click( ev =>{
     ev.preventDefault();
-     let newSection = document.createElement('SECTION');
-     let closeSpan = document.createElement('SPAN');
-     let newH1 = document.createElement('H1');
-     let pic = document.createElement('IMG');
-     pic.src = 'pictures/ScannedImage-7.jpg';
-     newSection.classList.add('onthefly');
-     closeSpan.innerText = 'X';
-     newH1.innerText ='Education';
-
-    newSection.append( closeSpan, newH1, pic);
-    $('body').append(newSection);
-
-    $('.onthefly span').click(function (ev){
-    $(this).parent().remove();
-
-   })
+    popup('ScannedImage-7','Education')
   })
   $('#projects').click( ev =>{
     ev.preventDefault();
-     let newSection = document.createElement('SECTION');
-     let closeSpan = document.createElement('SPAN');
-     let newH1 = document.createElement('H1');
-     let pic = document.createElement('IMG');
-     pic.src = 'pictures/code.jpg';
-     newSection.classList.add('onthefly');
-     closeSpan.innerText = 'X';
-     newH1.innerText ='Some Codes';
-
-    newSection.append( closeSpan, newH1, pic);
-    $('body').append(newSection);
-
-    $('.onthefly span').click(function (ev){
-    $(this).parent().remove();
-
-   })
+    popup('code','Some Codes')
   })
   $('#certifi').click( ev =>{
     ev.preventDefault();
-     let newSection = document.createElement('SECTION');
-     let closeSpan = document.createElement('SPAN');
-     let newH1 = document.createElement('H1');
-     let pic = document.createElement('IMG');
-     pic.src = 'pictures/ScannedImage-4.jpg';
-     newSection.classList.add('onthefly');
-     closeSpan.innerText = 'X';
-     newH1.innerText ='Some Certificates';
-
-    newSection.append( closeSpan, newH1, pic);
-    $('body').append(newSection);
-
-    $('.onthefly span').click(function (ev){
-    $(this).parent().remove();
-
-   })
+    popup('ScannedImage-4','Some Certificates')
   })
   $('#programes').click( ev =>{
     ev.preventDefault();
-     let newSection = document.createElement('SECTION');
-     let closeSpan = document.createElement('SPAN');
-     let newH1 = document.createElement('H1');
-     let pic = document.createElement('IMG');
-     pic.src = 'pictures/top.jpg';
-     newSection.classList.add('onthefly');
-     closeSpan.innerText = 'X';
-     newH1.innerText ='Programming Languages';
-
-    newSection.append( closeSpan, newH1, pic);
-    $('body').append(newSection);
-
-    $('.onthefly span').click(function (ev){
-    $(this).parent().remove();
-
-   })
+    popup('top','Programming Languages')
   })
   $('#lang_special').click( ev =>{
     ev.preventDefault();
-     let newSection = document.createElement('SECTION');
-     let closeSpan = document.createElement('SPAN');
-     let newH1 = document.createElement('H1');
-     let pic = document.createElement('IMG');
-     pic.src = 'pictures/skills.jpg';
-     newSection.classList.add('onthefly');
-     closeSpan.innerText = 'X';
-     newH1.innerText ='More Skills';
-
-    newSection.append( closeSpan, newH1, pic);
-    $('body').append(newSection);
-
-    $('.onthefly span').click(function (ev){
-    $(this).parent().remove();
-
-   })
+   popup('skills','More Skills')
   })
 })
