6e8411726ea6a558092c716b875236a745a472b1
unknown <orfali.murhaf@web.de>
2018-12-13 13:52:15 +0100

    make the onfly part in js file and add class for it in css

--- profile.js
+++ profile.js
@@ -23,5 +23,99 @@ $(document).ready(ev =>{
    counter = 0;
   })
 
-  
+  $('#education').click( ev =>{
+    ev.preventDefault();
+     let newSection = document.createElement('SECTION');
+     let closeSpan = document.createElement('SPAN');
+     let newH1 = document.createElement('H1');
+     let pic = document.createElement('IMG');
+     pic.src = 'pictures/ScannedImage-7.jpg';
+     newSection.classList.add('onthefly');
+     closeSpan.innerText = 'X';
+     newH1.innerText ='Education';
+
+    newSection.append( closeSpan, newH1, pic);
+    $('body').append(newSection);
+
+    $('.onthefly span').click(function (ev){
+    $(this).parent().remove();
+
+   })
+  })
+  $('#projects').click( ev =>{
+    ev.preventDefault();
+     let newSection = document.createElement('SECTION');
+     let closeSpan = document.createElement('SPAN');
+     let newH1 = document.createElement('H1');
+     let pic = document.createElement('IMG');
+     pic.src = 'pictures/code.jpg';
+     newSection.classList.add('onthefly');
+     closeSpan.innerText = 'X';
+     newH1.innerText ='Some Codes';
+
+    newSection.append( closeSpan, newH1, pic);
+    $('body').append(newSection);
+
+    $('.onthefly span').click(function (ev){
+    $(this).parent().remove();
+
+   })
+  })
+  $('#certifi').click( ev =>{
+    ev.preventDefault();
+     let newSection = document.createElement('SECTION');
+     let closeSpan = document.createElement('SPAN');
+     let newH1 = document.createElement('H1');
+     let pic = document.createElement('IMG');
+     pic.src = 'pictures/ScannedImage-4.jpg';
+     newSection.classList.add('onthefly');
+     closeSpan.innerText = 'X';
+     newH1.innerText ='Some Certificates';
+
+    newSection.append( closeSpan, newH1, pic);
+    $('body').append(newSection);
+
+    $('.onthefly span').click(function (ev){
+    $(this).parent().remove();
+
+   })
+  })
+  $('#programes').click( ev =>{
+    ev.preventDefault();
+     let newSection = document.createElement('SECTION');
+     let closeSpan = document.createElement('SPAN');
+     let newH1 = document.createElement('H1');
+     let pic = document.createElement('IMG');
+     pic.src = 'pictures/top.jpg';
+     newSection.classList.add('onthefly');
+     closeSpan.innerText = 'X';
+     newH1.innerText ='Programming Languages';
+
+    newSection.append( closeSpan, newH1, pic);
+    $('body').append(newSection);
+
+    $('.onthefly span').click(function (ev){
+    $(this).parent().remove();
+
+   })
+  })
+  $('#lang_special').click( ev =>{
+    ev.preventDefault();
+     let newSection = document.createElement('SECTION');
+     let closeSpan = document.createElement('SPAN');
+     let newH1 = document.createElement('H1');
+     let pic = document.createElement('IMG');
+     pic.src = 'pictures/skills.jpg';
+     newSection.classList.add('onthefly');
+     closeSpan.innerText = 'X';
+     newH1.innerText ='More Skills';
+
+    newSection.append( closeSpan, newH1, pic);
+    $('body').append(newSection);
+
+    $('.onthefly span').click(function (ev){
+    $(this).parent().remove();
+
+   })
+  })
 })
